ce6a2734fd34fb145b31bd508ebb04bce979cb32
Zain Eisa <zaineisa.work@gmail.com>
2021-03-14 16:22:55 +0000

    make it better

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 import plotly.express as px
-import boto3
+#import boto3
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
